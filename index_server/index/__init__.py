9c184ef657c9e9e22522cf7eaf1ee8d5865d268b
Durrah Azdi <124936827+durapoyo@users.noreply.github.com>
2026-04-12 21:40:25 -0400

    Add files via upload

---
+++ index_server/index/__init__.py
@@ -0,0 +1,21 @@
+import os
+from pathlib import Path
+
+from flask import Flask
+
+app = Flask(__name__)
+
+INDEX = {}
+PAGERANK = {}
+STOPWORDS = set()
+
+INDEX_DIR = Path(__file__).resolve().parent / "inverted_index"
+app.config["INDEX_PATH"] = os.getenv(
+    "INDEX_PATH",
+    str((INDEX_DIR / "inverted_index_1.txt").resolve()),
+)
+
+import index.api  # noqa: E402  pylint: disable=wrong-import-position
+
+# Load inverted index, stopwords, and pagerank into memory
+index.api.load_index()
