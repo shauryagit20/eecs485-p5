845f22ec0d4a660e7fac200e0b2a552f9e74a9bd
Arisha Zakry <arishasz@umich.edu>
2026-04-14 14:40:37 -0400

    everything

--- search_server/search/__init__.py
+++ search_server/search/__init__.py
@@ -1,7 +1,8 @@
 """Search server package."""
 import flask
-import search.model
-import search.views
 
 app = flask.Flask(__name__)
 app.config.from_object('search.config')
+
+import search.model  # noqa: E402  pylint: disable=wrong-import-position
+import search.views  # noqa: E402  pylint: disable=wrong-import-position
